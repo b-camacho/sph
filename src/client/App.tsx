b902d2976dade1bbe050760feea0230b33e23e8b
Brian Camacho <hello@briancamacho.me>
2024-11-26 00:47:18 -0500

    workaround safari 3rd party cookie restrictions

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -34,9 +34,10 @@ function App() {
     <Auth0Provider
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      cacheLocation="localstorage"
       authorizationParams={{
         redirect_uri: window.location.origin,
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
       }}
     >
       <BrowserRouter>
